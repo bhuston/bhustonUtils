ccb8770435e812b0704685d9cc80ed6f1b0608da
Ben Huston <bbhuston@gmail.com>
2015-11-15 22:54:31 -0500

    first update; clear default files

---
+++ R/add_stuff.R
@@ -0,0 +1,16 @@
+#' Add stuff
+#'
+#' @param first First this to add.
+#' @param second Second thing to add.
+#'
+#' @return first plus second thing
+#'
+#' @examples
+#' add_stuff( 10, 3)
+#' add_stuff( 20, 0)
+#'
+#' @export
+add_stuff <- function( first, second) {
+  first + second
+}
+
