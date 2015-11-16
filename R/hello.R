23bf2723b177197cfd626643acf83ab9a792e5d6
Ben Huston <bbhuston@gmail.com>
2015-11-15 21:54:29 -0500

    test commit status

--- R/hello.R
+++ R/hello.R
@@ -14,10 +14,16 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
-  print("Hello, world!")
+  print("Stuff it, world!")
 }
 
 
-#install.packages("formatR")
-#library(formatR)
+# #install.packages("formatR")
+# #library(formatR)
+#
+# devtools::install_github("bhuston/bhustonUtils")
+# library(bhustonUtils)
+# bhustonUtils::hello()
+#
+# devtools::check()
 
