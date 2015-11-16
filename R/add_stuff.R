cd4e17f64b4b394ac1e4738c2b8bf0fedd624c48
Ben Huston <bbhuston@gmail.com>
2015-11-15 23:52:33 -0500

    added example data

--- R/add_stuff.R
+++ R/add_stuff.R
@@ -1,15 +1,17 @@
 #' Add stuff
 #'
+#' Add stuff
+#'
+#' Add stuff
+#'
 #' @param first First this to add.
 #' @param second Second thing to add.
-#'
 #' @return first plus second thing
-#'
-#' @examples
-#' add_stuff( 10, 3)
-#' add_stuff( 20, 0)
-#'
 #' @export
 add_stuff <- function( first, second) {
   first + second
 }
+
+#' @examples
+#' add_stuff( 10, 3)
+#' add_stuff( 20, 0)
